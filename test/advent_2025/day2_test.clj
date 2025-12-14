0075528283e7a98c0d3132aa762421d0a515975f
Tom Simmons <tomasimmons@gmail.com>
2025-12-13 22:56:53 -0500

    solve 2025 day 2 part 2

--- test/advent_2025/day2_test.clj
+++ test/advent_2025/day2_test.clj
@@ -1,11 +1,14 @@
 (ns advent-2025.day2-test
   (:require [clojure.string :as str]
             [clojure.test :refer :all]
-            [advent-2025.day2 :refer [solve-part-1]]))
+            [advent-2025.day2 :refer [solve-part-1 solve-part-2]]))
 
 (deftest part1-solved
   (is (= 37314786486
          (solve-part-1 (slurp "resources/advent_2025/day2.part1.txt")))))
+(deftest part2-solved
+  (is (= 47477053982
+         (solve-part-2 (slurp "resources/advent_2025/day2.part1.txt")))))
 
 (deftest part1
   (testing "single-digit lower bounds"
@@ -26,3 +29,16 @@
          (solve-part-1
            (str/join ["11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,"
                      "38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"])))))
+
+(deftest part-2
+  (is (= (+ 11 111 1111
+            22 222
+            33 333
+            44 444
+            55 555
+            66 666
+            77 777
+            88 888
+            99 999
+            1010 1212 1313 1414 1515 1616 1717 1818 1919)
+         (solve-part-2 "1-2000"))))
