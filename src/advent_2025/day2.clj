a5426c70000eae92ac6d80e6dd0e9b24926900c9
Tom Simmons <tomasimmons@gmail.com>
2025-12-13 23:33:04 -0500

    Add perf notes

--- src/advent_2025/day2.clj
+++ src/advent_2025/day2.clj
@@ -21,3 +21,22 @@
 
 (defn solve-part-2 [line]
   (solver #"^(.+)(\1)+$" line))
+
+(comment
+  (use 'criterium.core)
+  ; How many numbers would we check with a brute-force approach?
+  ; About 2 million.
+  (let [input (slurp "resources/advent_2025/day2.part1.txt")]
+    (->> (str/split input #",")
+         (map (fn [range] (str/split range #"-")))
+         (map (fn [[low high]]
+                (inc (- (parse-long high) (parse-long low)))))
+         (reduce +)))
+  ; Our regex takes about ~280ns per execution (on my VM on my machine), so 2 million executions take about 0.5s.
+  ; Other regexes can run faster or slower, e.g. ^abc.*$ only need ~50ns, and could do more than 10 million executions
+  ; in half a second.
+  (let [input (map str (range 1))]
+    (with-progress-reporting (quick-bench
+                               (dorun (re-matches #"^(.+)(\1)+$" "12123412")))))
+  (let [input (slurp "resources/advent_2025/day2.part1.txt")]
+    (with-progress-reporting (quick-bench (solve-part-2 input)))))
