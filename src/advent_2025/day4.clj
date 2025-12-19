b4640972e6bae895100b9b3fcd2f2e8b59d0d676
Tom Simmons <tomasimmons@gmail.com>
2025-12-19 11:47:22 -0500

    Solve 2025 day 4 part 2 (sloppy)

--- src/advent_2025/day4.clj
+++ src/advent_2025/day4.clj
@@ -13,11 +13,11 @@
 ; Things that might change in part 2: I think the most likely thing is changing the definition of "accessible." That
 ; would not be hard to accommodate.
 ;
-
 ; Take the input file, read it into a vec of strings. This is a 2D matrix. For i,j within the bounds of the matrix,
 ; which we know to be 140 by 140, count the number of \@ characters in the surrounding 8 positions. Any position that
 ; falls outside the bounds is considered \. rather than \@. For each i,j where that sum is < 4, increment the counter.
 ; This is a reduction, as all problems apparently are :P
+
 (defn solve-part-1 [input]
   (cond
     (string? input)
@@ -47,6 +47,58 @@
     )
   )
 
+
+; Iteratively applying the brute-force algorithm to find rolls, remove them, and repeat until nothing changes will
+; multiply the 30x10^-6 cost by anywhere from a few hundred iterations to a few thousand. In the worst case we remove
+; one roll per iteration, taking ~20k iterations. That's 20x10^3 * 30x10^-6 = 600x10^-3 = 6x10^-1, around a second.
+; That's fine.
+(defn remove-rolls [input]
+  (let [d (count input)
+        d' (dec d)]
+    (->>
+      (for [y (range d) x (range d)]
+        (when (= \@ (nth (nth input y) x))
+          {:y y :x x :n
+           (->>
+             [[(dec y) (dec x)] [y (dec x)] [(inc y) (dec x)]
+              [(dec y) x] [(inc y) x]
+              [(dec y) (inc x)] [y (inc x)] [(inc y) (inc x)]]
+             (filter (fn [[y x]] (and (<= 0 x d') (<= 0 y d'))))
+             (filter (fn [[y x]] (= \@ (nth (nth input y) x))))
+             (count))}))
+      ; Find only accessible locations
+      (filter some?)
+      (filter #(< (:n %) 4))
+      (reduce
+        (fn [[result removed] {:keys [y x n]}]
+          (let [row (nth result y)]
+            [(assoc result y (assoc row x \x))
+             (inc removed)]))
+        [input 0])
+      ))
+  )
+
+(defn solve-part-2 [input]
+  (cond
+    (string? input)
+    ; 2D character vector. Strings support nth access, but not writing with (assoc). Hence, we use a vector.
+    (solve-part-2 (->> (str/split-lines input)
+                       (map #(vec (.toCharArray %)))
+                       (vec)))
+
+    (vector? input)
+    (loop [current input
+           removed 0]
+      (let [[next r] (remove-rolls current)]
+        (if (> r 0)
+          (recur next (+ removed r))
+          removed)))
+
+    :else
+    (throw (IllegalArgumentException. "Unexpected type of input -- should be string or vector"))
+    )
+  )
+
 (comment
   (solve-part-1 ["..@@.@@@@."
                  "@@@.@.@.@@"
@@ -58,4 +110,34 @@
                  "@.@@@.@@@@"
                  ".@@@@@@@@."
                  "@.@.@@@.@."])
-  (solve-part-1 (slurp "resources/advent_2025/day4.part1.txt")))
+  (solve-part-1 (slurp "resources/advent_2025/day4.part1.txt"))
+  (remove-rolls (vec (map #(vec (.toCharArray %)) ["..@@.@@@@."
+                                                   "@@@.@.@.@@"
+                                                   "@@@@@.@.@@"
+                                                   "@.@@@@..@."
+                                                   "@@.@@@@.@@"
+                                                   ".@@@@@@@.@"
+                                                   ".@.@.@.@@@"
+                                                   "@.@@@.@@@@"
+                                                   ".@@@@@@@@."
+                                                   "@.@.@@@.@."])))
+  (solve-part-2  (vec (map #(vec (.toCharArray %)) ["..@@.@@@@."
+                                                    "@@@.@.@.@@"
+                                                    "@@@@@.@.@@"
+                                                    "@.@@@@..@."
+                                                    "@@.@@@@.@@"
+                                                    ".@@@@@@@.@"
+                                                    ".@.@.@.@@@"
+                                                    "@.@@@.@@@@"
+                                                    ".@@@@@@@@."
+                                                    "@.@.@@@.@."])))
+  (solve-part-2 (slurp "resources/advent_2025/day4.part1.txt")))
+
+(comment
+  (use 'criterium.core)
+  (let [input (.toCharArray "..@@.@.@.@")]
+    (with-progress-reporting (quick-bench
+                               (reduce (fn [acc i] (assoc acc i "x")) (range (count input))))))
+  (let [input [true true false false true false true false true false]]
+    (with-progress-reporting (quick-bench
+                               (reduce (fn [acc i] (assoc acc i true)) (range (count input)))))))
