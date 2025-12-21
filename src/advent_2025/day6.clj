3037c8aa251d5b4061fcf0bbdf611c11e9903d53
Tom Simmons <tomasimmons@gmail.com>
2025-12-21 01:17:29 -0500

    solve 2025 day 6 part 2 + refactor day 1

--- src/advent_2025/day6.clj
+++ src/advent_2025/day6.clj
@@ -1,32 +1,83 @@
 (ns advent-2025.day6
   (:require [clojure.string :as str]))
 
-(defn parse [input]
-  (let [matrix (->> input
-                    (str/split-lines)
-                    (map #(re-seq #"[\+\*\d]+" %))
-                    (map #(into {} (map-indexed (fn [i v] {i v}) %)))
-                    (into {} (map-indexed (fn [i v] {i v})))
-                    )
-        max-y (apply max (keys matrix))
-        max-x (apply max (keys (get matrix 0)))]
-    (for [x (range (inc max-x))]
-      (reverse
-        (for [y (range (inc max-y))]
-          (if (< y max-y)
-            (parse-long (get-in matrix [y x]))
-            (keyword (get-in matrix [y x]))))))))
+(defn splits
+  "Creates a lazy sequence of strings by splitting the source string at each given index, beginning with from and
+   excluding the index itself."
+  ([source from [i & indices]]
+   (if (nil? i)
+     (cons (subs source from) (lazy-seq nil))
+     (cons (subs source from (dec i)) (lazy-seq (splits source i indices))))))
 
-(defn evaluate [[operation & operands]]
-  (case operation
-    :* (apply * operands)
-    :+ (apply + operands)))
+(str/join (concat "asdf" (repeat 3 \c)))
+
+(defn right-pad
+  "Returns a string of length n formed by concatenating s and a sufficient number of trailing characters c"
+  [s n c]
+  (str/join (concat s (repeat (- n (count s))
+                              c))))
+
+(defn columnize
+  "Returns a lazy sequence of lists, where each list represents a column from the problem read left-to-right and top-to-
+   bottom. Significant whitespace is preserved, but whitespace between columns of text is stripped."
+  [input]
+  (let [lines (str/split-lines input)
+        max-line-length (transduce (map count) max 0 lines)
+        ; account for my text editor removing trailing spaces that are significant to this problem!
+        lines (map #(right-pad % max-line-length \space) lines)
+        operator-line (last lines)
+        cs (.toCharArray operator-line)
+        col-indices (rest (for [i (range (count operator-line))
+                                :let [c (get cs i)]
+                                :when (not= \space c)]
+                            i))
+        columnized (map #(splits % 0 col-indices) lines)]
+    (partition (count lines) (apply interleave columnized))))
+
+(defn eval-col [[operator & operands]]
+          (case operator
+            :+ (apply + operands)
+            :* (apply * operands)))
 
 (defn solve-part-1 [input]
-  (->>
-    (parse input)
-    (map evaluate)
-    (reduce +))
+  (letfn [(parse-col [col]
+            (let [operator (keyword (last col))
+                  operands (->> (drop-last col)
+                                (map parse-long))]
+              (cons operator operands)))]
+    (->> input
+         (columnize)
+         (map #(map str/trim %))
+         (map parse-col)
+         (map eval-col)
+         (reduce + 0)))
   )
 
-(comment (solve-part-1 (slurp "resources/advent_2025/day6.txt")))
+(defn solve-part-2 [input]
+  (letfn [(transpose [mat] (apply map list mat))
+          (cephalopodize [col]
+            (let [operands (->> (drop-last col)
+                                (map #(.toCharArray %))
+                                (map seq))
+                  t (->> (transpose operands)
+                         (map (partial filter #(not= \space %)))
+                         (map str/join)
+                         (map parse-long))
+                  op (keyword (str/trim (last col)))]
+              (cons op t)))]
+    (let [x (->> input (columnize) (map cephalopodize) (vec))]
+      (->> input
+           (columnize)
+           (map cephalopodize)
+           (map eval-col)
+           (reduce + 0)
+           ))))
+
+(comment
+  (solve-part-2 (slurp "resources/advent_2025/day6.txt"))
+  (solve-part-2 (str/join "\n"
+                       ["123 328  51 64 "
+                        " 45 64  387 23 "
+                        "  6 98  215 314"
+                        "*   +   *   +  "]))
+  )
--- test/advent_2025/day6_test.clj
+++ test/advent_2025/day6_test.clj
@@ -1,27 +1,68 @@
 (ns advent-2025.day6-test
-  (:require [clojure.test :refer :all]
-            [advent-2025.day6 :refer [parse solve-part-1]]))
+  (:require [clojure.string :as str]
+            [clojure.test :refer :all]
+            [advent-2025.day6 :refer [solve-part-1 solve-part-2]]))
 
 (deftest part1
   (testing "multiplication"
-    (is (= 150 (solve-part-1 " 5
-                             3
-                            10
-                             *"))))
+    (is (= 150 (solve-part-1 (str/join "\n" [" 5"
+                                             " 3"
+                                             "10"
+                                             "* "])))))
   (testing "addition"
-    (is (= 18 (solve-part-1 " 5
-                            3
-                           10
-                            +"))))
+    (is (= 18 (solve-part-1 (str/join "\n" [" 5"
+                                            " 3"
+                                            "10"
+                                            "+ "])))))
   (testing "reduction"
-    (is (= 168 (solve-part-1 " 5  5
-                               3  3
-                              10 10
-                              *  +"))))
+    (is (= 168 (solve-part-1 (str/join "\n" [" 5  5"
+                                             " 3  3"
+                                             "10 10"
+                                             "*  + "])))))
   (testing "given input"
-    (is (= 4277556 (solve-part-1 "123 328  51  64
-                                   45  64 387  23
-                                    6  98 215 314
-                                  *   +   *   + "))))
+    (is (= 4277556 (solve-part-1 (str/join "\n"
+                                           ["123 328  51 64 "
+                                            " 45 64  387 23 "
+                                            "  6 98  215 314"
+                                            "*   +   *   +  "])))))
   (testing "solved part 1"
     (is (= 4771265398012 (solve-part-1 (slurp "resources/advent_2025/day6.txt"))))))
+
+(deftest part2
+  (testing "addition"
+    (is (= (+ (+ 1121 5869 7635 1)
+              (+ 339 4596 2986))
+           (solve-part-2 (str/join "\n" ["24   751"
+                                         "953  681"
+                                         "893  362"
+                                         "669 1591"
+                                         "+   +   "])))))
+  (testing "multiplication"
+    (is (= (+ (* 1121 5869 7635 1)
+              (* 339 4596 2986))
+           (solve-part-2 (str/join "\n" ["24   751"
+                                         "953  681"
+                                         "893  362"
+                                         "669 1591"
+                                         "*   *   "])))))
+  (testing "input shapes"
+    (is (= (+ (* 4 33 222 1111)
+              (+ 1111 222 33 4))
+           (solve-part-2 (str/join "\n" ["1234 4321"
+                                         "123   321"
+                                         "12     21"
+                                         "1       1"
+                                         "*    +   "])))))
+  (testing "text editor truncating trailing spaces like a little shit"
+    (is (= (* 4 33 222 1111)
+           (solve-part-2 (str/join "\n" ["1234"
+                                         "123"
+                                         "12"
+                                         "1"
+                                         "*"])))))
+  (testing "given input"
+    (is (= 3263827 (solve-part-2 (str/join "\n"
+                                           ["123 328  51 64 "
+                                            " 45 64  387 23 "
+                                            "  6 98  215 314"
+                                            "*   +   *   +  "]))))))
