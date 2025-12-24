fbe4849a197def5f02824ac6b845f17f40fc2bc9
Tom Simmons <tomasimmons@gmail.com>
2025-12-24 17:25:42 -0500

    Solve 2025 day 8 part 1

---
+++ src/advent_2025/day8.clj
@@ -0,0 +1,81 @@
+(ns advent-2025.day8
+  (:require [clojure.math :as math]
+            [clojure.string :as str]
+            [clojure.set :as set]))
+
+; The input contains 1000 lines, each line with the 3D integer coordinates of a junction box. We need to connect
+; together the 1000 nearest neighbors by Euclidian distance, forming one or more independent circuits of junction boxes.
+; Once done, we need to multiply the sizes of the 3 largest circuits, which is the answer to the puzzle.
+
+; There are 10^6 distinct pairs of junction boxes.
+; Euclidian distance takes roughly ~42ns (42x10^-9) on my machine
+; Getting the distance between all pairs should take milliseconds (10^-3).
+;
+; It appears that for any pair of junction boxes p1 and p2, the distance between the boxes of p1 is different from the
+; distance between the junction boxes of p2. This is at least true of the example and my problem input.
+;
+; Solution:
+; - Make a list of the 10^6 distinct pairs. Sort it by the Euclidian distance function.
+; - Take the first 1000 entries of the sorted list.
+; - Perform reduction:
+;     If neither junction box is already in a circuit set, add both to a new set. If just one is already
+;     in a circuit set, add both to the existing set. If both are in sets and those sets are the same, do nothing. If
+;     those sets are instead different, combine those sets together. The reduction state should be a set of circuit
+;     sets.
+;
+
+(defn sqr [x] (math/pow x 2))
+
+(defn distance [[x y z] [x' y' z']]
+  (math/sqrt (+ (sqr (- x' x)) (sqr (- y' y)) (sqr (- z' z)))))
+
+(defn cartesian-product [col]
+  (into #{} (for [x col y col :when (not= x y)]
+              #{x y})))
+
+(defn parse-coordinates
+  "Returns a lazy seq of coordinates vectors"
+  [input]
+  (->> (str/split-lines input)
+       (map #(re-seq #"\d+" %))
+       (map #(map parse-long %))
+       (map vec)))
+
+(defn sorted-connections
+  "Returns a sorted sequence of pairs of points, (#{p1 p2} #{p3 p4}...)"
+  [points]
+  (sort-by
+    (memoize #(apply distance %))
+    (cartesian-product points)))
+
+(defn solve-part-1
+  ([num-connections num-circuits input]
+   (let [boxes (parse-coordinates input)
+         to-connect (take num-connections (sorted-connections boxes))
+         circuits (reduce
+                    (fn [circuits circuit]
+                      (let [a (first circuit)
+                            b (second circuit)
+                            intersections (filter
+                                            #(or (contains? % a) (contains? % b))
+                                            circuits)]
+                        (case (count intersections)
+                          1 circuits                        ; circuit is a subset, so there's nothing to do.
+                          2 (conj (apply disj circuits intersections)
+                                  (apply set/union intersections)))))
+                    (into #{} (map (fn [x] #{x})) boxes)
+                    to-connect)
+         top-k-circuits (take num-circuits (reverse (sort-by count circuits)))]
+     (transduce
+       (map count)
+       *
+       1
+       top-k-circuits))))
+
+(comment
+  (solve-part-1 1000 3 (slurp "resources/advent_2025/day8.txt"))
+
+  (use 'criterium.core)
+  (with-progress-reporting
+    (quick-bench
+      (distance [21 15 8] [33 105 42]))))
---
+++ test/advent_2025/day8_test.clj
@@ -0,0 +1,51 @@
+(ns advent-2025.day8-test
+  (:require [clojure.string :as str]
+            [clojure.test :refer :all]
+            [advent-2025.day8 :refer [solve-part-1]]))
+
+(deftest part1-test
+  (testing "creates single-junction circuits if no connections are made"
+    (is (= 1 (solve-part-1 0 3 (str/join "\n" ["1,0,0", "2,0,0", "3,0,0"])))))
+
+  (testing "connects boxes in order of shortest distance"
+    ; This should connect 1-2, 4-5, and 7-8 into three circuits with 2 junctions each. 2*2*2 = 8.
+    (let [input (str/join "\n" ["1,0,0"
+                                "2,0,0"
+                                "4,0,0"
+                                "5,0,0"
+                                "7,0,0"
+                                "8,0,0"])]
+      (is (= 2 (solve-part-1 3 1 input)))
+      (is (= 4 (solve-part-1 3 2 input)))
+      (is (= 8 (solve-part-1 3 3 input)))))
+
+  (testing "can reduce to a single circuit"
+    ; 6 distinct pairs of connections can be made, though it's a single circuit after only 4 connections.
+    (let [input (str/join "\n" ["1,0,0"
+                                "2,0,0"
+                                "4,0,0"
+                                "8,0,0"])]
+      (is (= 4 (solve-part-1 4 1 input)))
+      (is (= 4 (solve-part-1 6 1 input)))))
+
+  (testing "given sample input"
+    (is (= 40 (solve-part-1 10 3 (str/join "\n" ["162,817,812"
+                                                 "57,618,57"
+                                                 "906,360,560"
+                                                 "592,479,940"
+                                                 "352,342,300"
+                                                 "466,668,158"
+                                                 "542,29,236"
+                                                 "431,825,988"
+                                                 "739,650,466"
+                                                 "52,470,668"
+                                                 "216,146,977"
+                                                 "819,987,18"
+                                                 "117,168,530"
+                                                 "805,96,715"
+                                                 "346,949,466"
+                                                 "970,615,88"
+                                                 "941,993,340"
+                                                 "862,61,35"
+                                                 "984,92,344"
+                                                 "425,690,689"]))))))
